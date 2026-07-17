0fd33fc05048f18eff13c9511d2f22fc4464422a
Sumit09988 <sumit999777555@gmail.com>
2026-07-17 09:24:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -123,7 +123,7 @@ def fetch_data(url):
 
 # 🔥 NEW STRUCTURE SUPPORT
 async def send_result(update, query):
-    url = f"https://tg2info.vercel.app/info?key=sumit996&id=thepaglu}"
+    url = f"https://tg2info.vercel.app/info?key=sumit996&id="
     data = fetch_data(url)
 
     if not data:
